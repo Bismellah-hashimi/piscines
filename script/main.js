cc1001db261d8be5876d527760f27dea74ffc5b8
bas <bas@gri.ch>
2025-06-26 14:03:29 +0200

    Fix : m3 calculator

--- script/main.js
+++ script/main.js
@@ -1,14 +1,20 @@
 
 function multiply() {
+    document.getElementById("error").innerHTML = "";
   var nombre1 = parseFloat(document.getElementById("nombre1").value);
   var nombre2 = parseFloat(document.getElementById("nombre2").value);
   var nombre3 = parseFloat(document.getElementById("nombre3").value);
-  var nombre4 = parseFloat(document.getElementById("nombre4").value);
   
-  if (isNaN(nombre1) || isNaN(nombre2) || isNaN(nombre3) || isNaN(nombre4)) {
-    document.getElementById("resultat").innerHTML = "Veuillez remplir tous les champs avec des nombres valides.";
+  if (isNaN(nombre1) || isNaN(nombre2) || isNaN(nombre3)) {
+    document.getElementById("error").innerHTML = "Veuillez remplir tous les champs avec des nombres valides.";
     return;
   }
-  var resultat = nombre1*nombre2*nombre3*nombre4
-  document.getElementById("resultat").innerHTML = resultat;
+  var resultat_m3 = nombre1*nombre2*nombre3;
+
+  console.log(resultat_m3);
+  document.getElementById("nombre4").value = resultat_m3;
+
+  var resultat_prix = resultat_m3 * 1000;
+  
+  document.getElementById("resultat").value = resultat_prix;
 }
